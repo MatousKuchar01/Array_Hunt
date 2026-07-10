3d4a97a1de56e3e3b012c11c2a4b66a794a5697d
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-07-10 11:22:04 +0200

    loot generator function

--- src/Generator/LootGenerator.php
+++ src/Generator/LootGenerator.php
@@ -1,8 +1,24 @@
 <?php
 
-use App\Generator;
+namespace App\Generator;
+
+use App\Enum\Loot;
 
 class LootGenerator
 {
-    
+    public static function drop(): Loot
+    {
+        $lootTable = Loot::rarityTable();
+        $chance = rand(1, 100);
+
+        if ($chance <= 5) {
+            $pool = $lootTable['legendary'];
+        } else if ($chance <= 20) {
+            $pool = $lootTable['rare'];
+        } else {
+            $pool = $lootTable['common'];
+        }
+
+        return $pool[array_rand($pool)];
+    }
 }
