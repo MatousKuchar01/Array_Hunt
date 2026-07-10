cdecbf7de1b3822d11f2fe3cb7e10dcc98022a44
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2026-07-10 13:37:44 +0200

    stubs for levels - preparing to build the algorithm

--- src/Generator/ArrayGeneratorV1.php
+++ src/Generator/ArrayGeneratorV1.php
@@ -6,6 +6,27 @@ class ArrayGeneratorV1
 {
     public static function generateLevel1()
     {
-        
+        return self::buildArray();
+    }
+
+    public static function generateLevel2()
+    {
+        return self::buildArray();
+    }
+
+    public static function generateLevel3()
+    {
+        return self::buildArray();
+    }
+
+    /**
+    * Builds random array with loot with various depth
+    * @param Loot $loot
+    * @param int $depth
+    * @return array
+    */
+    private static function buildArray(Loot $loot, int $depth = 0): array
+    {
+        //todo
     }
 }
